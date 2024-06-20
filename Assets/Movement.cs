4619b93d54b87828d563e60cdaea40864a774098
chris <krzysztof.malinski@cdv.pl>
2024-06-20 19:43:38 +0200

    ball and bouncing

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-
     public SpriteRenderer sprite;
     public Rigidbody2D rigid;
+
+    public float speed = 5f;
+    public KeyCode UpKey = KeyCode.W;
+    public KeyCode DownKey = KeyCode.S;
+   
 
     // Start is called before the first frame update
     void Start()
@@ -19,13 +23,13 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(UpKey) && transform.position.y < 4.5f)
         {
-            rigid.velocity = Vector2.up;
+            rigid.velocity = Vector2.up * speed;
         }
-        else if (Input.GetKey(KeyCode.S))
+        else if (Input.GetKey(DownKey) && transform.position.y > -4.5f)
         {
-            rigid.velocity = Vector2.down;
+            rigid.velocity = Vector2.down * speed;
         }
         else
         {
